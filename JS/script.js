f90144358937986e04272a44a95bf75f55523c6d
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-07-17 12:16:19 +0200

    feat: add generate square function

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,11 @@
 //stampo in pagina una grid di boxes con un ciclo
 //le box sono associate a numeri e vanno in coppie 
+//creare una funzione che generi 16 coppie di numeri random
+
+
+// FUNCTIONS 
+function createGrid(number) {
+    let card = document.createElement('div');
+    card.classList.add('box');
+    card.innterHtml += `<span>${number}</span>`
+}
