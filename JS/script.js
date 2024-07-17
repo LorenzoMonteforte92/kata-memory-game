a41461e8338e9376530758d759b4e2fd566fc841
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-07-17 17:54:46 +0200

    feat: add random pair of number function

--- JS/script.js
+++ JS/script.js
@@ -7,10 +7,11 @@
 
 //selezionare e mettere in una variabile il contenitore
 let mainContainer = document.querySelector(`#main-container`);
-console.log(mainContainer);
+
 //dichiarare fuori dal ciclo una variabile per il quadrato
+let randomArray = generatePairNumbers(16, 100)
 let newSquare
-for(let i = 1; i<=16; i++ ){
+for(let i = 1; i<=randomArray.length; i++ ){
     //passare la funzione in un ciclo for per stampare il numero desiderato di box
     newSquare = createGrid(i);
     //appendere in pagina l'elemento generato
@@ -19,9 +20,22 @@ for(let i = 1; i<=16; i++ ){
 
 
 // FUNCTIONS 
+
+//creates a grid of boxes
 function createGrid(number) {
     let card = document.createElement('div');
     card.classList.add('box');
     card.innterHtml += `<span>${number}</span>`
     return card;
+}
+
+//generates a pair of random numbers
+function generatePairNumbers(numOfPairs, max){
+    let pairNumbers = [];
+    for(let i = 0; i < numOfPairs; i++){
+        let randomNum = Math.floor(Math.random() * max);
+        pairNumbers.push([randomNum, randomNum]);
+    }
+    
+    return pairNumbers
 }
